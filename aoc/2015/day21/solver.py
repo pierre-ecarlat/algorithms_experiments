4647bf3d354ea6571e3db22663190b73cc4497dd
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2019-03-03 23:44:34 +0900

    aoc 2015 day 21 part2.... exactly one armor....

--- aoc/2015/day21/solver.py
+++ aoc/2015/day21/solver.py
@@ -4,8 +4,7 @@ import math
 
 shop = {
     'weapons': {
-        'none':       { 'cost': 0,  'damage': 0, 'armor': 0 },
-        'Dagger':     { 'cost': 8,  'damage': 5, 'armor': 0 },
+        'Dagger':     { 'cost': 8,  'damage': 4, 'armor': 0 },
         'Shortsword': { 'cost': 10, 'damage': 5, 'armor': 0 },
         'Warhammer':  { 'cost': 25, 'damage': 6, 'armor': 0 },
         'Longsword':  { 'cost': 40, 'damage': 7, 'armor': 0 },
@@ -20,7 +19,8 @@ shop = {
         'Platemail':  { 'cost': 102, 'damage': 0, 'armor': 5 },
     },
     'rings': {
-        'none':       { 'cost': 0,   'damage': 0, 'armor': 0 },
+        'none1':      { 'cost': 0,   'damage': 0, 'armor': 0 },
+        'none2':      { 'cost': 0,   'damage': 0, 'armor': 0 },
         'Damage_+1':  { 'cost': 25,  'damage': 1, 'armor': 0 },
         'Damage_+2':  { 'cost': 50,  'damage': 2, 'armor': 0 },
         'Damage_+3':  { 'cost': 100, 'damage': 3, 'armor': 0 },
@@ -45,18 +45,19 @@ player_stats = {
 
 def possible_combinations():
     combinations = []
-    for kw, vw in shop['weapons'].items():
-        for ka, va in shop['armors'].items():
+    for vw in shop['weapons'].values():
+        for va in shop['armors'].values():
             for kr1, vr1 in shop['rings'].items():
                 for kr2, vr2 in shop['rings'].items():
-                    if not (kr1 == kr2 and kr1 != 'none'):
-                        combinations.append({
-                            'cost': sum(x['cost'] for x in [vw, va, vr1, vr2]),
-                            'attack': sum(x['damage'] for x in [vw, vr1, vr2]),
-                            'defense': sum(x['armor'] for x in [va, vr1, vr2])
-                        })
+                    if kr1 == kr2:
+                        continue
+                    combinations.append({
+                        'cost': sum(x['cost'] for x in [vw, va, vr1, vr2]),
+                        'attack': sum(x['damage'] for x in [vw, vr1, vr2]),
+                        'defense': sum(x['armor'] for x in [va, vr1, vr2])
+                    })
     combinations.sort(key = lambda x: x['cost'])
-    return combinations
+    return list(combinations)
 
 def simulate_game(boss, player):
     player_damages = max(player['attack'] - boss['defense'], 1)
@@ -67,10 +68,14 @@ def simulate_game(boss, player):
 
     return turns_to_die_player >= turns_to_die_boss
 
+min_cost = 999
+max_cost = 0
 wearables = possible_combinations()
 for wearable in wearables:
-    player_stats['attack'] = wearable['attack']
-    player_stats['defense'] = wearable['defense']
-    if simulate_game(boss_stats, player_stats) == True:
-        print(wearable)
-        break
+    player_stats.update(wearable)
+    if simulate_game(boss_stats, player_stats):
+        min_cost = min(wearable['cost'], min_cost)
+    else:
+        max_cost = max(wearable['cost'], max_cost)
+
+print('{} {}'.format(min_cost, max_cost))
